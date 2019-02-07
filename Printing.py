8fa0fe918dcc86311dcb1749e819b6563591470d
Alex Diker <alex.diker@live.ca>
2019-02-06 19:51:04 -0500

    Update Printing.py

--- Printing.py
+++ Printing.py
@@ -51,4 +51,22 @@ years = [
     "1997",
 ]
 
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
+        + ['st', 'nd', 'rd'] + 7 * ['th'] \
+        + ['st']
+
+year = raw_input('Year:  ')
+month = raw_input('Month (1-12): ')
+day = raw_input('Day (1-31): ')
+
+month_number = int(month)
+day_number = int(day)
+
+# Remember to subtract 1 from month and day to get the correcet index
+
+month_name = months[month_number-1]
+ordinal = day + endings[day_number-1]
+
+print month_name + ' ' + ordinal + ', ' + year
+
 raw_input("\nPress <enter> to end application")
