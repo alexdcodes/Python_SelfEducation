afc09ff0e0dd5bf1a58a4e2d5f6ba23aceb1bd9a
Alex Diker <alex.diker@live.ca>
2019-02-06 19:42:18 -0500

    Add files via upload

---
+++ Printing.py
@@ -0,0 +1,54 @@
+from math import sqrt
+
+print ("Hello world\n\n")
+print sqrt(10)
+
+name = raw_input("What is your name? ")
+print "Hello,  " + name + "!"
+print "\nPython Scripts for testing\n\n"
+x = "Hello"
+y = "World"
+
+print (y, x)
+
+temp = "42"
+print "The temperature is " + temp
+
+print '''\n THIS IS A JUST A TESTING APPLICATION FOR EDUCATION NOTHING MORE, .
+.. EXAMPLE OF A VERY LONG STRING PROGRAM IN PYTHON'''
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+    "October",
+    "November",
+]
+
+years = [
+    "1984",
+    "1985",
+    "1986",
+    "1987",
+    "1988",
+    "1989",
+    "1990",
+    "1991",
+    "1992",
+    "1993",
+    "1994",
+    "1995",
+    "1996",
+    "1997",
+]
+
+raw_input("\nPress <enter> to end application")
