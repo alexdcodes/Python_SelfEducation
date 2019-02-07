ce8dc9057d039b88219eff965823299aa389e23e
Alex D <alex.diker@live.ca>
2019-02-07 11:55:02 -0500

    Update loops_otherstatements.py

--- loops_otherstatements.py
+++ loops_otherstatements.py
@@ -12,3 +12,17 @@ if name.endswith('Alex'):
         print 'Hello, Alex'
 else:
     print 'Hello, Stranger'
+
+
+def enter_number():
+
+    num = input("Enter a number: ")
+    if num > 0:
+        print 'The number is positive'
+    elif num < 0:
+        print 'The number is negative'
+    else:
+        print 'The number is zero'
+
+        
+enter_number()
