64ffffbdd7a3897dde8781f766297f8510146fab
Alex D <alex.diker@live.ca>
2019-02-09 20:14:18 -0500

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,11 @@
+def describeanimal(animal):
+    print 'Description of', animal['name']
+    print 'Age:', animal['age']
+    try:
+        print 'Type of Animal: ', + animal['occupation']
+    except KeyError: pass
+
+
 class MuffledCalculator:
     def __init__(self):
         pass
@@ -9,10 +17,12 @@ class MuffledCalculator:
             return eval(expr)
         except ZeroDivsionError:
             if self.muffled:
-                print "Division by zero is denied"
+                print "Division by zero is illegal"
             else:
                 raise
 
 
+# Zen of Exceptions
 calculator = MuffledCalculator()
+
 print calculator.calc('10/2')
