6db4cb3454eddf39a53790cf0d138f4ef1d93f6c
Alex D <alex.diker@live.ca>
2019-02-09 20:29:14 -0500

    Update magic_methods.py

--- magic_methods.py
+++ magic_methods.py
@@ -1,8 +1,25 @@
 # Before: you should either put the assignment __metaclass__ = type at the top of your modules
 
+
 class FooBar:
     def __init__(self):
         self.somevar = 42
 
+
+class Bird:
+    def __init__(self):
+        self.hungry = True
+
+    def eat(self):
+        if self.hungry:
+            print 'Aaah...'
+            self.hungry = False
+        else:
+            print 'No, thanks!'
+
+
 f = FooBar()
+b = Bird()
 print f.somevar
+print b.eat()
+print b.eat()
