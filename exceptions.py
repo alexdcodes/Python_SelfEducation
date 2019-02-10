8b3e2a0637bd89e8a1047012c7de188e453b566f
Alex D <alex.diker@live.ca>
2019-02-09 19:45:05 -0500

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+class MuffledCalculator:
+    def __init__(self):
+        pass
+
+    muffled = False
+
+    def calc(self, expr):
+        try:
+            return eval(expr)
+        except ZeroDivsionError:
+            if self.muffled:
+                print "Division by zero is illegal"
+            else:
+                raise
+
+
+calculator = MuffledCalculator()
+print calculator.calc('10/2')
