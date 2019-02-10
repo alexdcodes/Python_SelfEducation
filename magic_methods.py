651cb8b6c34beed8bb16465d7e04fce6de00a26c
Alex D <alex.diker@live.ca>
2019-02-10 14:55:18 -0500

    Update magic_methods.py

--- magic_methods.py
+++ magic_methods.py
@@ -28,6 +28,24 @@ class BarkDog(Bird):
         print self.sound
 
 
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        
+    def __setattr__(self, name, value):
+        if name == 'size':
+            sef.width, self.height = value
+        else:
+            self.__dict__[name] = value
+
+    def __getattr__(self, name):
+        if name == 'size':
+            return self.width  , self.height
+        else:
+            raise AttributeError
+
+
 f = FooBar()
 b = Bird()
 sb = BarkDog()
