3882fcb77347e5a71f42e0fc5cb8993762476995
Alex D <alex.diker@live.ca>
2019-02-10 16:13:01 -0500

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,8 +1,10 @@
 import math
-
 import sys
+import webbrowser
 
 # sys.path.append('c:/python')  # For Windows-based systems
 sys.path.append('~/python')  # For Unix-based systems
 
 print math.sin(0)
+webbrowser.open('https://google.com')  # Opens Web Browser
+
