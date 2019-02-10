3f41e397e67875e291f60a6bf12ae255b8350348
Alex D <alex.diker@live.ca>
2019-02-10 17:48:39 -0500

    Update templates.py

--- templates.py
+++ templates.py
@@ -6,4 +6,21 @@ import fileinput, re
 field_pat = re.compile(r'\[(.+?)\]')
 
 
-# We'll collect variables in this: 
+# We'll collect variables in this:
+scope = {}
+
+# This is used in re.sub:
+
+
+def replacement(match):
+    code = match.group(1)
+    try:
+        # If the field can be evaluated, return it:
+        return str(eval(code, scope))
+    except SyntaxError:
+        # Otherwise execute teha ssignment in the same scope....
+        exec code in scope
+        # ...and return an empty string:
+        return ''
+
+# Get all the text as a single string:
