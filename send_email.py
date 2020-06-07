0ea11d5d61493634dc413eec6cabd42ba7faf1fd
Alex Diker <alex.diker@live.ca>
2020-06-06 20:07:32 -0400

    update
    update

--- send_email.py
+++ send_email.py
@@ -1,6 +1,6 @@
 import smtplib
-sender_mail = 'sender@fromdomain.com'
-receivers_mail = ['reciever@todomain.com']
+sender_mail = 'sender@mydomain.com'
+receivers_mail = ['reciever@mydomaim.com']
 message = """Well this is just an exaple
 of an email script for eduaction purposes 
 """%(sender_mail,receivers_mail)
